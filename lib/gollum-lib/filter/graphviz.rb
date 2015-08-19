150cbd73e6d9280b7078e0d118cb1bf3d9c0b953
Matt Hill <matthewchill7@yahoo.com>
2015-08-18 23:59:45 -0400

    Duh... I copy/pasted the config file

--- lib/gollum-lib/filter/graphviz.rb
+++ lib/gollum-lib/filter/graphviz.rb
@@ -1,82 +1,48 @@
-# Remove const to avoid
-# warning: already initialized constant FORMAT_NAMES
-#
-# only remove if it's defined.
-# constant Gollum::Page::FORMAT_NAMES not defined (NameError)
-Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
-# Limit Formats
-Gollum::Page::FORMAT_NAMES = { :markdown  => "Markdown", :asciidoc => "AsciiDoc" }
+class Gollum::Filter::GRAPHVIZ < Gollum::Filter
+  # Extract all graphviz blocks into the map and replace with placeholders
+  def extract(data)
+    return data if @markup.format == :txt
+    data.gsub(/^\[graphviz(?:, ?(.+?))?\]\r?\n----\r?\n(.+?)\r?\n----$/m) do
+      id       = Digest::SHA1.hexdigest(Regexp.last_match[2])
+      @map[id] = { :style => Regexp.last_match[1], :code => Regexp.last_match[2] }
+      id
+    end
+  end
 
-=begin
-Valid formats are:
-{ :markdown  => "Markdown",
-  :textile   => "Textile",
-  :rdoc      => "RDoc",
-  :org       => "Org-mode",
-  :creole    => "Creole",
-  :rest      => "reStructuredText",
-  :asciidoc  => "AsciiDoc",
-  :mediawiki => "MediaWiki",
-  :pod       => "Pod" }
-=end
+  # Process all diagrams from the map and replace the placeholders with
+  # the final HTML.
+  #
+  # data - The String data (with placeholders).
+  #
+  # Returns the marked up String data.
+  def process(data)
+    @map.each do |id, spec|
+      data.gsub!(id) do
+        render_wsd(spec[:code], spec[:style])
+      end
+    end
+    data
+  end
 
-# Specify the path to the Wiki.
-gollum_path = '/root/docs'
-Precious::App.set(:default_markup, :asciidoc)
+  private
+  # Render the sequence diagram on the remote server.
+  #
+  # Returns an <img> tag to the rendered image, or an HTML error.
+  def render_wsd(code, style)
+    if style.nil?
+      style = "dot"
+    end
 
-# Specify the wiki options.
-wiki_options = {
-  :css              => true,
-  :js               => false,
-  #:template_dir     => path,
-  #:page_file_dir    => path,
-  :ref              => 'master',
-  :repo_is_bare     => false,
-  :allow_editing    => true,
-  :live_preview     => true,
-  :allow_uploads    => true,
-  :per_page_uploads => false,
-  :mathjax          => true,
-  #:mathjax_config   => source,
-  :user_icons       => 'gravatar',
-  :collapse_tree    => false,
-  :h1_title         => true,
-  :show_all         => false,
-  :universal_toc    => false,
-  :filter_chain     => [:Metadata, :PlainText, :TOC, :RemoteCode, :GRAPHVIZ, :Code, :Macro, :Sanitize, :WSD, :Tags, :Render]
-}
-Precious::App.set(:wiki_options, wiki_options)
-Precious::App.set(:gollum_path, gollum_path)
+    title = Digest::MD5.hexdigest(code)
 
-# Set as Sinatra environment as production (no stack traces)
-Precious::App.set(:environment, :production)
-
-# Setup Omniauth via Omnigollum.
-require 'omnigollum'
-require 'omniauth-ldap'
-
-options = {
-  # OmniAuth::Builder block is passed as a proc
-  :providers => Proc.new do
-    provider :ldap,
-      :title => "SSZ LDAP",
-      :host => 'sszldap01.ssz.tekelec.com',
-      :port => 389,
-      :method => :plain,
-      :base => 'ou=ssz,dc=tekelec,dc=com',
-      :uid => 'uid'
-  end,
-  :dummy_auth => false,
-  # Specify committer name as just the user name
-  :author_format => Proc.new { |user| user.name },
-  # Specify committer e-mail as just the user e-mail
-  :author_email => Proc.new { |user| user.email },
-  # Only allow DVAT to alter files
-  :authorized_users => ["dmayo", "gkannaiy", "igowtham", "jfedor", "jposton", "mchill", "rbodford", "rhew"]
-}
-
-# :omnigollum options *must* be set before the Omnigollum extension is registered
-Precious::App.set(:omnigollum, options)
-Precious::App.register Omnigollum::Sinatra
-
+    if not File.exist?('/root/docs/uploads/graphviz/' + title + '.png')
+      File.write('/root/tmp/graph.txt', code)
+      %x(dot2ruby /root/tmp/graph.txt > /root/tmp/graph.rb)
+      %x(echo ".output(:use => '#{style}', :png => '/root/docs/uploads/graphviz/#{title}.png')" >> /root/tmp/graph.rb)
+      %x(ruby /root/tmp/graph.rb)
+      %x(cd /root/docs;git add uploads/graphviz/*;git commit -m "New Graphviz Image")
+    end
+    return '<img src="/uploads/graphviz/' + title + '.png">'
+  end
+end
 
